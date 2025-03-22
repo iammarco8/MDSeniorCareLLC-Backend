96a000509fda023275924a3022b170e9d28dda49
Marco Duhaney <iammarco8@gmail.com>
2025-03-21 22:11:40 -0500

    database.js changed

--- data/database.js
+++ data/database.js
@@ -4,10 +4,10 @@ import dot from 'dotenv';
 dot.config({path:"./config.env"});
 
 export const pool = mysql.createPool({
-    host:sql5.freesqldatabase.com,
-    user:sql5768920,
-    password:BKJZyxJaq6,
-    database:sql5768920
+    host:'sql5.freesqldatabase.com',
+    user:'sql5768920',
+    password:'BKJZyxJaq6',
+    database:'sql5768920'
     // host:process.env.DB_HOST,
     // user:process.env.DB_USER,
     // password:process.env.DB_PASS,
